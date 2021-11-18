2b74758f4cbba7d97045428f3db7ac11c60334df
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-18 09:58:27 +0100

    Pas compris exo 3 :(

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -2,6 +2,18 @@ let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
+colorEl.innerHTML = 'noir';
+colorEl.style.border = '1px solid black';
+
+handEl.innerHTML = 'Roman Reigns';
+handEl.style.fontStyle= 'Arial';
+handEl.style.color = 'white';
+handEl.style.backgroundColor = 'Blue';
+
+frameEl.style.border = '1px solid red';
+
+
+
 
 
 
